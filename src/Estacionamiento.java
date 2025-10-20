cf3b798489b86a91f93b1c74446b99fca1ba6c37
evaro <evarodriguezbarrera2006@gmail.com>
2025-10-20 11:58:25 +0200

    mas o menos los metodos de estacionamiento

--- src/Estacionamiento.java
+++ src/Estacionamiento.java
@@ -1,20 +1,40 @@
 import java.util.ArrayList;
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
 
 public class Estacionamiento {
     private final int capacidadMax=5;
     private final Semaphore semaphore = new Semaphore(5);
     private final ArrayList<Coche> cochesAparcados = new ArrayList<>();
 
-    boolean entrar (Coche coche){
+    synchronized boolean entrar (Coche coche){
+        if (cochesAparcados.size() < capacidadMax){
+            cochesAparcados.add(coche);
+            return true;
+        } else {
+            if (coche.esVip()){
+                desalojarCocheNormal(coche);
+                cochesAparcados.add(coche);
+                return true;
+            } else {
+                semaphore.tryAcquire();
+            }
+        }
         return false;
     }
 
-    void salir (Coche coche){
-
+    synchronized void salir (Coche coche){
+        cochesAparcados.remove(coche);
+        semaphore.release();
     }
 
-    void desalojarCocheNormal(Coche cocheVip){
-
+    void desalojarCocheNormal (Coche cocheVip) {
+        if (cochesAparcados.size() < capacidadMax) {
+            for (int i = 0; i < cochesAparcados.size(); i++) {
+                if (!cochesAparcados.get(i).esVip()) {
+                    salir(cochesAparcados.get(i));
+                }
+            }
+        }
     }
 }
