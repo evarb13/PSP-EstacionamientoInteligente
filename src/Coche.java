167c1baf4732ac69326c4c26ed6352ecadeda472
evaro <evarodriguezbarrera2006@gmail.com>
2025-10-23 17:10:37 +0200

    clase coche hecha

--- src/Coche.java
+++ src/Coche.java
@@ -1,10 +1,25 @@
-public class Coche {
+public class Coche extends Thread{
     private String nombre;
     private boolean vip;
     private Estacionamiento estacionamiento;
 
     boolean esVip(){
-        return false;
+        return vip;
+    }
+
+    @Override
+    public void run(){
+        boolean aparcado = estacionamiento.entrar(this);
+        double numero = (Math.random()* 4 + 2);
+
+        try{
+            if (aparcado){
+                Thread.sleep((long)(numero*1000));
+                estacionamiento.salir(this);
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
